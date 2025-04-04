9cf46a20e37d7708bf72637ca8573bbb7bd0c5f5
rs0657 <rs0657@srmist.edu.in>
2025-04-04 17:35:51 +0530

    changes in api.js made

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `https://${process.env.URL}/api`;  // Updated port to match new backend port
+const API_URL = `${process.env.REACT_APP_API_URL}/api`;  // Updated to use REACT_APP prefix
 
 // Fetch all products
 export const fetchProducts = async () => {
